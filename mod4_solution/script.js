b7a29476a34d671b59da33fa15fabba6196c659b
Alicja Brylka <brylka.ala@gmail.com>
2016-11-20 21:06:33 +0100

    module 4 - solution

---
+++ mod4_solution/script.js
@@ -0,0 +1,16 @@
+(function () {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  for (prop in names) {
+
+    var firstLetter = names[prop].charAt(0);
+
+    if(firstLetter.toLowerCase() == "j"){
+      byeSpeaker.speak(names[prop]);
+    }
+
+    else{
+      helloSpeaker.speak(names[prop]);
+    }
+  }
+}) ();
